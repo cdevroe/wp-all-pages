9c770e5695c29f531c4930f5c5918a1fa2200903
Colin Devroe <colin@cdevroe.com>
2016-10-12 16:00:12 -0400

    Initial commit.

---
+++ all-pages.php
@@ -0,0 +1,22 @@
+<?php /**
+ * Plugin Name: Add Pages to "All Pages"
+ * Description: Creates a sub-menu under "All Pages" in Admin with current site's pages
+ * Author: Colin Devroe
+ * Author URI: http://cdevroe.com/
+ * Version: 1.0.0
+*/
+
+// Add Menu item in navigation
+function cdevroe_all_pages_navigation(){
+  //add_submenu_page( 'Payment Form Link Builder', 'Payment Form Link Builder', 'manage_options', 'clb-form', 'clb_admin_page' );
+  $pages = get_pages(array('post_status'=>'publish'));
+  foreach ($pages as $page_data) {
+    add_submenu_page( 'edit.php?post_type=page', 'post.php?post='.$page_data->ID.'&action=edit', '- '.$page_data->post_title, 'manage_options', $page_data->ID, function(){} );
+  }
+}
+add_action( 'admin_menu', 'cdevroe_all_pages_navigation' );
+
+function cdevroe_all_pages_admin_scripts(){
+  wp_enqueue_script( 'cdevroe-all-pages-js', plugins_url() . '/all-pages/all-pages.js' );
+}
+add_action( 'admin_enqueue_scripts', 'cdevroe_all_pages_admin_scripts' );
---
+++ readme.md
@@ -0,0 +1,7 @@
+Add All Pages to WordPress Admin Navigation
+---------------------------------------------
+
+A simple WordPress plugin that adds all published pages to the WordPress Admin menu under "All Pages". It was Jono Young's idea on Twitter: https://twitter.com/chsweb/status/786280565414567936
+
+1.0.0 - October 12, 2016
+  - Initial commit
